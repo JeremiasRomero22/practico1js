23da0c8a96c9a641b8c681a113b81daf6b02d84f
Jeremias Romero <jereromero2610@gmail.com>
2023-10-30 01:28:57 -0300

    realice el ejercicio siete y ocho del campus

--- js/ejercicios2.js
+++ js/ejercicios2.js
@@ -14,9 +14,31 @@ console.log("La suma de los números es: " + (numero1 + numero2))
 var numero1 = (prompt("Ingrese el primer número:"))
 var numero2 = (prompt("Ingrese el segundo número:"))
 if (numero1 > numero2) {
-  console.log("El " + numero1 + " es el número mayor.")
+  console.log("El " + numero1 + " es el número más grande.")
 } else if (numero2 > numero1) {
-  console.log("El " + numero2 + " es el número mayor.")
+  console.log("El " + numero2 + " es el número más grande.")
 } else {
-  console.log("Ambos números son iguales.")
+  console.log("Los números son iguales.")
 }
+
+//7
+var numero1 = (prompt("Ingrese el primer número:"))
+var numero2 = (prompt("Ingrese el segundo número:"))
+var numero3 = (prompt("Ingrese el tercer número:"))
+if (numero1 >= numero2 && numero1 >= numero3) {
+  console.log("El " + numero1 + " es el número más grande.")
+} else if (numero2 >= numero1 && numero2 >= numero3) {
+  console.log("El " + numero2 + " es el número más grande.")
+} else {
+  console.log("El " + numero3 + " es el número más grande.")
+}
+
+//8
+var numero = (prompt("Ingrese un número:"))
+if (numero % 2 === 0) {
+  console.log("El ${numero}es divisible por 2")
+} else {
+  console.log("El"+numero+"no es divisible por 2.")
+}
+
+//9
